0c59d382fef15a424523e215f959d505554af723
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2022-04-21 15:46:09 +0300

    init
    init

---
+++ modules-and-routing-app/src/app/customer/customer-list/customer-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CustomerService, IUser } from '../customer.service';
+
+@Component({
+  selector: 'app-customer-list',
+  templateUrl: './customer-list.component.html',
+  styleUrls: ['./customer-list.component.scss']
+})
+export class CustomerListComponent implements OnInit {
+
+  customers: IUser[] = [];
+
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit(): void {
+    this.customerService.getUsers$().subscribe(
+      users => {
+        this.customers = users;
+      }
+    );
+  }
+
+}
